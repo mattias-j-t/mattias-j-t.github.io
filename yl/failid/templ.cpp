fddd4d743cbe8a7e1ef64e3f54175d96feceb417
mattias-j-t <167644598+mattias-j-t@users.noreply.github.com>
2026-02-28 00:56:22 +0200

    adfsf

---
+++ yl/failid/templ.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define vi vector<int>
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL)
+#define FOR(i,a,b) for (int i = (a);i<(b);i++) //for i in range(a,b)
+#define debug(x) cout << #x << " = " << x << "\n"
